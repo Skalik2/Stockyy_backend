3881ef1704fc6da6b4e8d78dfa812a72f9cb6e79
Mateusz Skali <mateuszskali@gmail.com>
2025-05-05 18:14:46 +0200

    Fix start_flask.sh to run Flask in the background and redirect output to log file

--- start_flask.sh
+++ start_flask.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-nohup flask run 
+nohup flask run > flask.log 2>&1 &
 
-echo $! > flask.pid
+echo $! > flask.pid 
 
 echo "Flask uruchomiony w tle. Logi: flask.log, PID: $(cat flask.pid)"
